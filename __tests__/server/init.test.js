d2845a2ab05b074e8ba13344504aff3b45a160d2
Matheus Lima <limabrot879@gmail.com>
2020-07-20 18:18:49 -0300

    Update init.test.js

--- __tests__/server/init.test.js
+++ __tests__/server/init.test.js
@@ -5,11 +5,10 @@ describe('Server', function () {
     it('Server Successful Inited', async () => {
         try {
             await server.start();
-            status = 1
         } catch (error) {
             console.error('Erro:', error);
-            status = 0
+            status = 1
         }
-        expect(status).toBe(1);
+        expect(status).toBe(0);
     })
 })
